a6fdbe91d14f2b627fc15fdefbed13829784870c
sathsara-cw <sathsara.sarathchandra@cloudways.com>
2023-04-09 10:39:36 +0530

    Declared missing variables

--- kubernetes-cluster/src/workload-identity/variables.tf
+++ kubernetes-cluster/src/workload-identity/variables.tf
@@ -3,6 +3,11 @@ variable "env_project_id" {
   description = "The project id to create WIF pool and SA"
 }
 
+variable "region" {
+  description = "GCP region name"
+  type        = string
+}
+
 variable "service_account_name" {
   type        = string
   description = "The service account name"
@@ -16,4 +21,9 @@ variable "github_username" {
 variable "github_repo_name" {
   type        = string
   description = "GitHub repo name to grant access"
+}
+
+variable "artifact_registry_repo_name" {
+  type        = string
+  description = "Name of the GCP artifact registry repository"
 }
