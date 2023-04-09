b7acbecfd5460e31d1373be6fc5ce0ae3a6408f2
sathsara-cw <sathsara.sarathchandra@cloudways.com>
2023-04-09 12:34:27 +0530

    updated go app

--- demo-app/main.go
+++ demo-app/main.go
@@ -1,42 +1,50 @@
-// This app is based on https://github.com/olliefr/docker-gs-ping
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/semconv"
 )
 
 func main() {
-
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
-
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("PORT")
-	if httpPort == "" {
-		httpPort = "8080"
+	// Create an OTLP exporter and register it as a trace provider.
+	ctx := context.Background()
+	exporter, err := otlp.NewExporter(ctx, otlpgrpc.NewDriver(
+		otlpgrpc.WithInsecure(),
+		otlpgrpc.WithEndpoint(os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")),
+	))
+	if err != nil {
+		log.Fatal(err)
 	}
+	provider := trace.NewTracerProvider(
+		trace.WithSpanProcessor(
+			trace.NewBatchSpanProcessor(exporter),
+		),
+		trace.WithResource(
+			semconv.ServiceNameKey.String(os.Getenv("OTEL_SERVICE_NAME")),
+		),
+	)
+	otel.SetTracerProvider(provider)
 
-	e.Logger.Fatal(e.Start(":" + httpPort))
-}
+	// Set up the web server and start listening for requests.
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		tracer := otel.Tracer("my-go-app")
+		_, span := tracer.Start(ctx, "handleRequest")
+		defer span.End()
 
-// Simple implementation of an integer minimum
-// Adapted from: https://gobyexample.com/testing-and-benchmarking
-func IntMin(a, b int) int {
-	if a < b {
-		return a
+		// Handle the request.
+		fmt.Fprintf(w, "Hello, world!\n")
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
-	return b
 }
